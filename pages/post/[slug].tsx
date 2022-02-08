f13c5e5913cf6bbd98ed8f09fbd91e2af900d894
Harsh Johar <hpsj3112@gmail.com>
2022-02-09 03:10:01 +0530

    post page done

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,130 @@
+import { GetStaticProps } from 'next'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
+interface Props {
+  post: Post
+}
+
+const Post = ({ post }: Props) => {
+  return (
+    <main>
+      <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={
+          //@ts-ignore
+          urlFor(post.mainImage).url()!
+        }
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            src={urlFor(post.author.image).url()!}
+            alt=""
+            className="h-10 w-10 rounded-full object-contain"
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="cursor-pointer text-green-600">
+              {post.author.name}
+            </span>
+            - Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className='mt-10'>
+          <PortableText
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="list-desc ml-4">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+            content={post.body}
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+          />
+        </div>
+      </article>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type=="post"] {
+        _id,
+      slug {
+        current
+      }
+      }`
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type=="post" && slug.current== $slug][0] {
+        _id,
+        _createdAt,
+        title,
+        author-> {
+        name, 
+        image
+      },
+      'comments': *[
+        _type=="comment" && post._ref==^._id && approved==true
+      ],
+      description,
+      mainImage,
+      slug,
+      body
+      }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // after 60 seconds, it ll update old cached version
+  }
+}
